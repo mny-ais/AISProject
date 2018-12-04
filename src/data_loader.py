f03d8b773fcbe6828b2f45c3d0070d35378fb89f
Bachelor project AIS group 3 <aisgrp3@tfpool61.informatik.uni-freiburg.de>
2018-12-04 11:10:18 +0100

    commented out torch

--- src/data_loader.py
+++ src/data_loader.py
@@ -5,13 +5,13 @@
 from __future__ import print_function, division
 import os
 import os.path as op
-import torch
+# import torch
 import pandas as pd
 from skimage import io, transform
 import numpy as np
 import matplotlib.pyplot as plt
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms, utils
+# from torch.utils.data import Dataset, DataLoader
+# from torchvision import transforms, utils
 
 # Ignore warnings
 import warnings
