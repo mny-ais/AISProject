97e3e480ccd20b50737ae4e602431a849e94d815
Mxtreme1 <mr492@students.uni-freiburg.de>
2019-01-07 19:06:55 +0100

    Added data augmentation (Nina proud)

--- src/data_loader.py
+++ src/data_loader.py
@@ -12,6 +12,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
+import imgaug as ia
+from imgaug import augmenters as iaa
 
 
 # Ignore warnings
@@ -74,9 +76,32 @@ class ToTensor(object):
         # swap color axis because
         # numpy image: H x W x C
         # torch image: C X H X W
+        [B,G,R] = np.dsplit(image,image.shape[-1])
+
         image = image.transpose((2, 0, 1))
+
         return {'image': torch.from_numpy(image),
                 'drive_data': torch.from_numpy(drive_data)}
+
+"""
+The Image Augmentations
+courtesy: https://github.com/mvpcom/carlaILTrainer/blob/master/carlaTrain.ipynb
+"""
+
+st = lambda aug: iaa.Sometimes(0.4, aug)
+oc = lambda aug: iaa.Sometimes(0.3, aug)
+rl = lambda aug: iaa.Sometimes(0.09, aug)
+seq = iaa.Sequential([
+        rl(iaa.GaussianBlur((0, 1.5))), # blur images with a sigma between 0 and 1.5
+        rl(iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05), per_channel=0.5)), # add gaussian noise to images
+        oc(iaa.Dropout((0.0, 0.10), per_channel=0.5)), # randomly remove up to X% of the pixels
+        oc(iaa.CoarseDropout((0.0, 0.10), size_percent=(0.08, 0.2),per_channel=0.5)), # randomly remove up to X% of the pixels
+        oc(iaa.Add((-40, 40), per_channel=0.5)), # change brightness of images (by -X to Y of original value)
+        st(iaa.Multiply((0.10, 2.5), per_channel=0.2)), # change brightness of images (X-Y% of original value)
+        rl(iaa.ContrastNormalization((0.5, 1.5), per_channel=0.5)), # improve or worsen the contrast
+        #rl(iaa.Grayscale((0.0, 1))), # put grayscale
+], random_order=True)
+
 
 
 transformed_dataset = DrivingSimDataset(csv_file='test.csv',
@@ -90,8 +115,6 @@ for i in range(len(transformed_dataset)):
 
     print(i, sample['image'].size(), sample['drive_data'].size())
 
-    if i == 3:
-        break
 
 """
 image_num = csv_file.iloc[0, 0]
