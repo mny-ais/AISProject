20966f337386cae36eab0b571a20a23bce85038a
Yvan Satyawan <y_satyawan@hotmail.com>
2019-01-21 21:06:56 +0100

    Gave up on image mover

--- src/utils/image_mover.py
+++ src/utils/image_mover.py
@@ -33,7 +33,10 @@ def move_images(parent_dir):
         parent_dir (str): Parent directory which contains all the recordings.
     """
     count = 0
+    csv_line_count = 0
     total = 0
+
+    print("Processing {:0>5d}/{}".format(count, total))
 
     dir_list = []  # List of directories with images, excluding "all"
 
@@ -48,8 +51,6 @@ def move_images(parent_dir):
                 cur_dir = path.join(parent_dir, dir)
                 dir_list.append(cur_dir)
                 total += len(listdir(cur_dir))
-
-    print("Processing: {:0>5d}/{}".format(0, total))
 
     all_dir = path.join(parent_dir, 'all')
 
@@ -66,6 +67,9 @@ def move_images(parent_dir):
     csv_data = []
     if path.exists(path.join(all_dir, "control_input.csv")):
         write_mode = "a"  # Append if it already exists
+    else:
+        write_mode = "w"  # Write new if non existent
+
         csv_data.append(
             ["No.",
              "Steering",
@@ -73,54 +77,59 @@ def move_images(parent_dir):
              "Brakes",
              "Gear",
              "Requested_Direction"]
-        )
-    else:
-        write_mode = "w"  # Write new if non existent
-    print(listdir(dir_list[2]))
+        )  # Add header if non existant
 
     for dir in dir_list:
         # Get a 0th index for the current folder
+        file_list = listdir(dir)
+        num_files = len(file_list)
+        image_count = 0
 
-        for file in listdir(dir):
+        for file in file_list:
             # File is currently only the file name, without the whole path
             # dir contains the whole path to the current working directory
             if file.endswith(".csv"):
                 with open(path.join(dir, file), mode='r') as old_csv:
                     reader = csv.reader(old_csv)
-                    csv_data.append(list(reader))
-                    print(csv_data[0])
+                    line_list = list(reader)[1:]
+                    for line in line_list:
+                        data = line
+                        data[0] = int(csv_line_count) + int(data[0])
+                        csv_data.append(data)
+
+                    csv_line_count += len(line_list)
                 old_csv.close()
 
                 with open(path.join(all_dir, "control_input.csv"),
-                                    mode=write_mode) as csv_file:
+                                    mode=write_mode, newline='') as csv_file:
                     writer = csv.writer(csv_file)
                     writer.writerows(csv_data)
                 csv_file.close()
                 csv_data = []
-            """
+                write_mode = 'a'
 
             if file.endswith(".png"):
                 new_name = ""
+
                 # First extract the number values and convert them to int
                 num_vals = re.findall(p, file)
-                num_vals[0] = int(num_vals[0])
+                num_vals[0] = int(num_vals[0]) + int(count)
                 num_vals[1] = int(num_vals[1])
 
+                # put the max value as image_count
+                image_count = max(image_count, num_vals[0])
                 # Now the 2 cases of seg or image
                 if file.startswith("image"):
-                    new_name = "image_{:0>5d}-cam_{}.png".format(num_vals[0]
-                                                                 + int(count),
+                    new_name = "image_{:0>5d}-cam_{}.png".format(num_vals[0],
                                                                  num_vals[1])
                 elif file.startswith("seg"):
-                    new_name = "seg_{:0>5d}-cam_{}.png".format(num_vals[0]
-                                                               + count,
+                    new_name = "seg_{:0>5d}-cam_{}.png".format(num_vals[0],
                                                                num_vals[1])
 
                 rename(path.join(dir, file), path.join(all_dir, new_name))
-            """
-        count += len(listdir(dir))
 
-
+        count += image_count
+        print("Processing {:0>5d}/{}".format(count, total))
 
 
 
